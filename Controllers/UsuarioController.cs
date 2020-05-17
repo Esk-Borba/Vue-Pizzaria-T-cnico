3a202ac40ed8ef2ae55cead02fed966131381f3f
Esk-Borba <vinicius_fuhr-borba@estudante.sc.senai.br>
2020-05-17 09:58:11 -0300

    att

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -31,18 +32,48 @@ namespace Sa_pro_chefe.Controllers
         }
 
         // POST: api/Usuario
-        public void Post([FromBody]string value)
+        public string Post([FromBody]dados_cliente user)
         {
+           using(pro_chef_webEntities bd = new pro_chef_webEntities())
+            {
+                bd.dados_cliente.Add(user);
+                bd.SaveChanges();
+                return "Salvo com sucesso";
+            }
         }
 
         // PUT: api/Usuario/5
-        public void Put(int id, [FromBody]string value)
+        public string Put(int id, [FromBody]dados_cliente user)
         {
+            using(pro_chef_webEntities bd = new pro_chef_webEntities())
+            {
+                dados_cliente userAlterar = bd.dados_cliente.Find(id);
+                userAlterar.cpf = user.cpf;
+                userAlterar.nome = user.nome;
+                userAlterar.telefone = user.telefone;
+                userAlterar.usuario = user.usuario;
+                userAlterar.senha = user.senha;
+                userAlterar.cep = user.cep;
+                userAlterar.logradouro = user.logradouro;
+                userAlterar.complemento = user.complemento;
+                userAlterar.bairro = user.bairro;
+                userAlterar.cidade = user.cidade;
+                userAlterar.uf = user.uf;
+                bd.SaveChanges();
+                return "Salvo com Sucesso";
+            }
         }
 
         // DELETE: api/Usuario/5
-        public void Delete(int id)
+        public string Delete(int id)
         {
+            using(pro_chef_webEntities bd = new pro_chef_webEntities())
+            {
+                bd.dados_cliente.Remove(bd.dados_cliente.Find(id));
+                bd.SaveChanges();
+                return "Removido com sucesso";
+            }
+            
         }
     }
 }
